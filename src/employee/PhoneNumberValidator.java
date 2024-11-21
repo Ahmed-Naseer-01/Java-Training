6dc968b3603356eaf10a2d045d6f0c04e5e7f446
Ahmed Naseer <codeblooded69@gmail.com>
2024-11-21 15:49:06 +0500

    chore : remove unnecessary code snippets

--- src/employee/PhoneNumberValidator.java
+++ src/employee/PhoneNumberValidator.java
@@ -6,8 +6,6 @@ import java.util.regex.*;
 public class PhoneNumberValidator {
 
     private static Properties countryCodes = new Properties();
-
-    // Load the country codes and regex patterns from the properties file
     public static void loadCountryCodes() {
         try (FileReader fis = new FileReader("src/employee/country_codes.properties")) {
             countryCodes.load(fis);
@@ -17,11 +15,8 @@ public class PhoneNumberValidator {
         }
 
     }
-    public  static boolean validateCountryCode(String countryCode)
-    {
-//        loadCountryCodes();
+    public  static boolean validateCountryCode(String countryCode) {
         String regex = countryCodes.getProperty(countryCode);
-//        System.out.println("Regex for country code " + countryCode + ": " + regex.substring());
 
         if (regex == null) {
             System.out.println("No phone number validation pattern found for country code: " + countryCode);
@@ -30,20 +25,11 @@ public class PhoneNumberValidator {
         return  true;
     }
     public static boolean validatePhoneNumber(String countryCode, String phoneNumber) {
-        // Retrieve the regex pattern for the given country code
         Scanner sc = new Scanner(System.in);
         String regex = countryCodes.getProperty(countryCode);
-//
-//        if (regex == null) {
-//            System.out.println("No phone number validation pattern found for country code: " + countryCode);
-//            return false;
-//        }
-        // System.out.println("Enter your Phone number start with your country code " + countryCode);
         Pattern phonePattern = Pattern.compile(regex);
-       //String cNum = sc.nextLine();
         Matcher matcher = phonePattern.matcher(phoneNumber);
         if (matcher.matches()) return true;
         else return false;
     }
     }
-
