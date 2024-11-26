43cd2a64a9df0e423a05d73cdfd783961162d9d7
Ahmed Naseer <codeblooded69@gmail.com>
2024-11-26 21:41:51 +0500

    add : define constant file

---
+++ src/employee/Constant.java
@@ -0,0 +1,10 @@
+package employee;
+
+public class Constant {
+    static final String ROOT = "root";
+    static final String PASS = "123ahmed";
+    static final String DATE_FORMAT = "dd-MM-yyyy";
+    static final String TEMPADDRESS = "Temporary";
+    static final String PERMADDRESS = "Permanent";
+//    private static final String DATE_PATTERN = "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4})$";
+}
