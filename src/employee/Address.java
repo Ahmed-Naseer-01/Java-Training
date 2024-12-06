325e5779b7eb4420209686d67b61ba4adcd2178e
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-06 11:20:00 +0500

    Fix : Rename file and define new attributes

---
+++ src/employee/Address.java
@@ -0,0 +1,59 @@
+package employee;
+
+public class Address {
+    private int id;
+
+    private String street;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+}
