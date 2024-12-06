2f8666151f8d2b66a37189fccf90284e6f8eb90a
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-06 14:55:15 +0500

    Add : Address Type class

--- src/employee/Constant.java
+++ src/employee/Constant.java
@@ -4,7 +4,10 @@ public class Constant {
     static final String ROOT = "root";
     static final String PASS = "123ahmed";
     static final String DATE_FORMAT = "dd-MM-yyyy";
-    static final String TEMPADDRESS = "Temporary";
-    static final String PERMADDRESS = "Permanent";
+    // Enum to define address types
+    public enum AddressType {
+        TEMPORARY,
+        PERMANENT
+    }
 //    private static final String DATE_PATTERN = "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4})$";
 }
