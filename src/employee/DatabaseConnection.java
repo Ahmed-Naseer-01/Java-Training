3737df71c90e4973535b51bddadc49c8a356e87d
code Blooded <108016919+Ahmed-Naseer-01@users.noreply.github.com>
2024-12-09 01:46:10 +0500

    Fix : Update CRUD operation

--- src/employee/DatabaseConnection.java
+++ src/employee/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/employee_schema";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Employee_schema";
     private static final String USER = Constant.ROOT;
     private static final String PASSWORD = Constant.PASS;
 
